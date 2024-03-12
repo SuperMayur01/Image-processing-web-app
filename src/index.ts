5b1de259a2637bd9313ca4c548aff9f5a443f593
Mayur Raina <mayur.raina@gmail.com>
2024-03-12 23:43:37 +0530

    removed

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import app from "./app";
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-app.listen(process.env.PORT, () => {
-    console.log(`Application started`);
-  });
--- src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { handleRequest, validateQuery } from './api/image';
-const router = express.Router();
-
-//route to get processed image
-router.get('/image', validateQuery, handleRequest);
-
-export default router;
